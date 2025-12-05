d0bd3cc2c997ed4c42902bc8a4e7b6fb0bce8bfe
prajjwal ujjaini <prajjwalujjaini@gmail.com>
2025-12-05 18:14:52 +0530

    3rd build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -65,7 +65,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "995f6677865aa3b9cb0d4c04c079847b
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "3a253b30d52bd7da5e5ee05dca0314ad",
+"flutter_bootstrap.js": "b83be6a79def4f6165bba6ed0f496ee6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
